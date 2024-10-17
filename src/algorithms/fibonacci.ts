89cf34a57941ab45283782969a0547f2dd51d37c
cyber9ja <ayodejioladapo15@gmail.com>
2024-10-17 22:35:24 +0100

    Updated Fibonacci sequence

--- src/algorithms/fibonacci.ts
+++ src/algorithms/fibonacci.ts
@@ -1,25 +1,31 @@
 // My Solution
-// function fibonacci(n: number): number[] {
-//   let array: number[] = [];
-
-//   for (let i = 0; i < n; i++) {
-//     if( i <= 1) array.push(i);
-//     else{
-//       array.push(array[array.length - 1] + array[array.length - 2]);
-//     }
-//   }
-//   return array;
-// }
-
 function fibonacci(n: number): number[] {
   let fib: number[] = [0, 1];
+
   for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-    // fib.push(fib[i - 1] + fib[i - 2]);
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
   }
   return fib;
 }
 
+// function fibonacci(n: number): number[] {
+//   let fib: number[] = [0, 1];
+//   for (let i = 2; i < n; i++) {
+//     fib[i] = fib[i - 1] + fib[i - 2];
+//     // fib.push(fib[i - 1] + fib[i - 2]);
+//   }
+//   return fib;
+// }
 
 
+
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
+console.log(fibonacci(7));
 console.log(fibonacci(8));
+console.log(fibonacci(9));
+console.log(fibonacci(10));
