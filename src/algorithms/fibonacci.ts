228024073ceb69a2069fc84adbdc4bd19455be97
cyber9ja <ayodejioladapo15@gmail.com>
2024-10-21 18:04:31 +0100

    Added fibonacci

--- src/algorithms/fibonacci.ts
+++ src/algorithms/fibonacci.ts
@@ -1,7 +1,6 @@
 // My Solution
 function fibonacci(n: number): number[] {
   let fib: number[] = [0, 1];
-
   for (let i = 2; i < n; i++) {
     fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
   }
@@ -17,15 +16,13 @@ function fibonacci(n: number): number[] {
 //   return fib;
 // }
 
+function recursiveFibonacci(n: number): number {
+  console.log(n);
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
 
-
-console.log(fibonacci(1));
-console.log(fibonacci(2));
-console.log(fibonacci(3));
-console.log(fibonacci(4));
-console.log(fibonacci(5));
-console.log(fibonacci(6));
-console.log(fibonacci(7));
-console.log(fibonacci(8));
-console.log(fibonacci(9));
-console.log(fibonacci(10));
+// console.log(fibonacci(6));
+console.log(recursiveFibonacci(6));
